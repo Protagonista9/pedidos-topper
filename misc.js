0fc387c13ce1dddd2686b9d1c658f7d3a9bdbc59
Protagonista9 <renner0p@gmail.com>
2026-05-29 16:38:43 -0300

    Update misc.js

--- misc.js
+++ misc.js
@@ -1,3 +1,4 @@
+
 function toggleTheme() {
     const body = document.body;
     const current = body.getAttribute('data-theme');
@@ -20,4 +21,27 @@ function setLoading(loading) {
 
 function encodeWhatsApp(texto) {
     return encodeURIComponent(texto);
+}
+
+function limitarIdade(input) {
+    let valor = input.value;
+
+    let numeros = valor.match(/\d+/g);
+
+    if (numeros) {
+        let numero = numeros.join('').slice(0, 3);
+
+        // remove zero inicial
+        numero = numero.replace(/^0+/, '');
+
+        // limita até 125
+        if (numero && Number(numero) > 125) {
+            numero = '125';
+        }
+
+        valor = valor.replace(/\d+/g, '');
+        input.value = (numero + valor).slice(0, 15);
+    } else {
+        input.value = valor.slice(0, 15);
+    }
 }
